08cbaf730e71c2ad6e28b90bd62345e0a012c470
VictoriaChou <zhoufuxing@sjtu.edu.cn>
2017-07-10 21:13:28 +0800

    inter programming

--- lp.py
+++ lp.py
@@ -1,12 +1,13 @@
+# coding=utf-8
 import numpy as np
 import matplotlib.pyplot as mpl
-from scipy import optimize
+from scipy.optimize import linprog
 
 c = np.array([1,1,1])
 a = np.array([3.05,4.05,6.1])
 b = np.array([7.9])
 
-res = optimize.linprog(c,-a,b)
+res = linprog(c,-a,b)
 
 print (res.x)
 
